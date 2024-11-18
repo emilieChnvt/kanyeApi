5408d494419c08cc64f4355911e8cef3b0c9d969
Emilie <emilie.chnvt@gmail.com>
2024-11-18 17:51:32 +0100

    eventListener added on click btnQuote

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
-let url = 'https://api.kanye.rest'
+
+const btnQuote = document.querySelector('.quote');
+
+btnQuote.addEventListener('click', showQuote);
+
+function showQuote(){
+    let url = 'https://api.kanye.rest'
 
 
-fetch(url)
-.then(response => response.json())
-.then(json => console.log(json))
+    fetch(url)
+        .then(response => response.json())
+        .then(json => console.log(json))
+
+
+}
+
+
+
