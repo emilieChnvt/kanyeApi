0cecaa79639fed70969db4c950e0d9bbd4c2bf97
Emilie <emilie.chnvt@gmail.com>
2024-11-18 18:53:06 +0100

    .quoteContainer instead of .quote-container

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
 
 const containerBtn = document.querySelector('.containerBtn');
 const btnQuote = document.querySelector('.quote');
+const quoteContainer = document.querySelector('.quoteContainer');
 
 
 
 btnQuote.addEventListener('click', recupereQuote);
 
 function recupereQuote(){
-    let url = 'https://api.kanye.rest'
+    const url = 'https://api.kanye.rest';
    fetch(url)
         .then(response => response.json())
-        .then(json => containerBtn.innerHTML += json.quote)
+        .then(json => {
+            quoteContainer.innerHTML = ''; // clear last quote
+
+            let newDiv = document.createElement('div');
+            newDiv.innerHTML = json.quote;
+            quoteContainer.appendChild(newDiv);
+            }
+        )
 }
 
 
